a931496558243a6bae345ca9bf237a5ebb690e87
natali <erohova.nat@yandex.ru>
2021-12-13 16:15:08 +0300

    created file for home work two

---
+++ HomeWorkTwo.java
@@ -0,0 +1,38 @@
+/**
+* Java 1. Homework #2
+*
+* @author Natalia Erokhova
+* @version 13.12.2021
+*/
+class HomeWorkTwo {
+    public static void main (String[] args) {
+        System.out.println (comparSum (25, -20));
+        System.out.println (comparSum (10, 5));
+        printNum (34);
+        printNum (0);
+        printNum (-5);
+        System.out.println (comparNum (12));
+        System.out.println (comparNum (0));
+        System.out.println (comparNum (-12));
+        printLine ("Two", 3);
+        printLine ("Lesson 2", 1);
+    }
+    
+    static boolean comparSum (int a, int b) {
+        return ((a + b) > 10 && (a + b) < 20) ? true : false;
+    }
+    
+    static void printNum (int a) {
+        System.out.println (a >= 0 ? "Number is positive" : "Number is negative");
+    }
+    
+    static boolean comparNum (int a) {
+        return a < 0 ? true : false;
+    }
+    
+    static void printLine (String word, int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println (word);
+        }
+    }
+}
